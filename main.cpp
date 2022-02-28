f9c26998ff2a55c88d5e9eea62ce262d61f3d283
z8Tygas <61952605+z8Tygas@users.noreply.github.com>
2022-02-28 02:18:24 +0000

    Good cam rotation implem

--- main.cpp
+++ main.cpp
@@ -8,13 +8,27 @@
 #include <math.h>
 
 //VARS GLOB
-float transx = 0;
-float transz = 0;
-float escala = 1;
-int rotate = 0;
-float camx = 20;
-float camz = 20;
-int mode = 0;
+//object trans rotate e scale
+	float transx = 0;
+	float transz = 0;
+	float escala = 1;
+	int rotate = 0;
+//object draw mode
+	int mode = 0;
+//cam motion
+	float camx = 20;
+	float camy = 0;
+	float camz = 0;
+	float alpha = M_PI_2;
+	float beta = 0;
+//mouse vars
+	int oldx = 0;
+	int oldy = 0;
+//cam
+	int enable_rotate = 0;
+	int enable_trans = 0;
+	int invert_cam = 1;
+	int cam_sense = 250; //higher -> less sens
 
 void changeSize(int w, int h) {
 
@@ -49,7 +63,7 @@ void renderScene(void) {
 
 	// set the camera
 	glLoadIdentity();
-	gluLookAt(camx,20.0,camz, 
+	gluLookAt(camx, camy, camz,
 		      0.0,0.0,0.0,
 			  0.0f,1.0f,0.0f);
 	
@@ -147,12 +161,53 @@ void interp_teclado(unsigned char key, int x, int y) {
 	glutPostRedisplay();
 }
 
+void interp_rato_click(int button, int state, int x, int y) {
+	//LMB Controls
+	if (button == 0 && state == GLUT_DOWN) {
+		enable_trans = 1;
+		oldx = x;
+		oldy = y;
+	}
+	else if (button == 0 && state == GLUT_UP) {
+		enable_trans = 0;
+	}
+	//RMB Controls
+	else if (button == 2 && state == GLUT_DOWN) {
+		enable_rotate = 1;
+		oldx = x;
+		oldy = y;
+	}
+	else if (button == 2 && state == GLUT_UP) {
+		enable_rotate = 0;
+	}
+}
+
 void interp_rato_motion(int x, int y) {
-	camx = 20*sin(  ((float) x / GLUT_INIT_WINDOW_WIDTH) * 2 * M_PI  );
-	camz = 20*sin(  ((float) y / GLUT_INIT_WINDOW_WIDTH) * 2 * M_PI  );
+	if (enable_rotate) {
+		float varx = oldx - x;
+		float vary = oldy - y;
+		oldx = x;
+		oldy = y;
 
+		varx /= cam_sense;
+		vary /= cam_sense;
+		alpha += varx;
+		if (invert_cam) {
+			beta -= vary;
+		}
+		else {
+			beta += vary;
+		}
 
-	glutPostRedisplay();
+		camx = 20 * cos(beta) * sin(alpha);
+		camz = 20 * cos(beta) * cos(alpha);
+		camy = 20 * sin(beta);
+
+		glutPostRedisplay();
+	}
+	else if (enable_trans) {
+
+	}
 }
 
 
@@ -174,8 +229,7 @@ int main(int argc, char **argv) {
 // put here the registration of the keyboard callbacks
 	glutKeyboardFunc(interp_teclado);
 	glutMotionFunc(interp_rato_motion);
-
-
+	glutMouseFunc(interp_rato_click);
 
 
 //  OpenGL settings

d9f480de4da33022f83cd02afd80e81f19f24710
Joel Pinto <61952605+z8Tygas@users.noreply.github.com>
2022-03-08 11:48:06 +0000

    Update DrawCylinder.cpp

--- DrawCylinder.cpp
+++ DrawCylinder.cpp
@@ -1,9 +1,3 @@
-#ifdef __APPLE__
-#include <GLUT/glut.h>
-#else
-#include <GL/glut.h>
-#endif
-
 #define _USE_MATH_DEFINES
 #include <math.h>
 void drawCylinder(float radius, float height, int slices) {

4e187ca123bdc63cd454d8fcb2b04b61d5912a42
Joel Pinto <61952605+z8Tygas@users.noreply.github.com>
2022-03-22 09:42:20 +0000

    Update DrawCylinder.cpp

--- DrawCylinder.cpp
+++ DrawCylinder.cpp
@@ -1,41 +1,38 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
-void drawCylinder(float radius, float height, int slices) {
-	float a = 0;
-	float angle = 2*M_PI / slices;
-	//draw bottom base
-	for (int i = 0; i < slices; i++) {
-		glBegin(GL_TRIANGLES);
-		glColor3f(i%2, i%2, i%2);
-		glVertex3f(0, 0, 0);
-		glVertex3f(radius * sin(a + angle * (i+1)), 0, radius * cos(a + angle * (i+1)));
-		glVertex3f(radius * sin(a + angle * i), 0, radius * cos(a + angle * i));
-		glEnd();
+void drawCylinder(float radius, float height, int sides) {
+
+	int i;
+	float step;
+
+	step = 360.0 / sides;
+
+	glBegin(GL_TRIANGLES);
+
+	// top
+	for (i = 0; i < sides; i++) {
+		glVertex3f(0, height * 0.5, 0);
+		glVertex3f(cos(i * step * M_PI / 180.0) * radius, height * 0.5, -sin(i * step * M_PI / 180.0) * radius);
+		glVertex3f(cos((i + 1) * step * M_PI / 180.0) * radius, height * 0.5, -sin((i + 1) * step * M_PI / 180.0) * radius);
 	}
-	//draw top base
-	a = angle / 2;
-	for (int i = 0; i < slices; i++) {
-		glBegin(GL_TRIANGLES);
-		glColor3f(i % 2, i % 2, i % 2);
-		glVertex3f(0, height, 0);
-		glVertex3f(radius * sin(a + angle * i), height, radius * cos(a + angle * i));
-		glVertex3f(radius * sin(a + angle * (i + 1)), height, radius * cos(a + angle * (i + 1)));
-		glEnd();
+
+	// bottom
+	for (i = 0; i < sides; i++) {
+		glVertex3f(0, -height * 0.5, 0);
+		glVertex3f(cos((i + 1) * step * M_PI / 180.0) * radius, -height * 0.5, -sin((i + 1) * step * M_PI / 180.0) * radius);
+		glVertex3f(cos(i * step * M_PI / 180.0) * radius, -height * 0.5, -sin(i * step * M_PI / 180.0) * radius);
 	}
-	//draw sides
-	a = 0;
-	for (int i = 0; i < slices; i++) {
-		glBegin(GL_TRIANGLES);
-		// Bottom Up
-		glColor3f(i % 2, i % 2, i % 2);
-		glVertex3f(radius * sin(a + angle * i),				0, radius * cos(a + angle * i));
-		glVertex3f(radius * sin(a + angle * (i+1)),			0, radius * cos(a + angle * (i + 1)));
-		glVertex3f(radius * sin((a + angle / 2) + angle * i),	   height, radius * cos((a + angle / 2) + angle * i));
-		// Top Down
-		glColor3f(i % 2, i % 2, i % 2);
-		glVertex3f(radius * sin((a + angle / 2) + angle * (i+1)), height , radius * cos((a + angle / 2) + angle * (i + 1)));
-		glVertex3f(radius * sin((a + angle / 2) + angle * i),	  height , radius * cos((a + angle / 2) + angle * i));
-		glVertex3f(radius * sin(a + angle * (i+1)),			0, radius * cos(a + angle * (i+1)));
-		glEnd();
+
+	// body
+	for (i = 0; i <= sides; i++) {
+
+		glVertex3f(cos(i * step * M_PI / 180.0) * radius, height * 0.5, -sin(i * step * M_PI / 180.0) * radius);
+		glVertex3f(cos(i * step * M_PI / 180.0) * radius, -height * 0.5, -sin(i * step * M_PI / 180.0) * radius);
+		glVertex3f(cos((i + 1) * step * M_PI / 180.0) * radius, height * 0.5, -sin((i + 1) * step * M_PI / 180.0) * radius);
+
+		glVertex3f(cos(i * step * M_PI / 180.0) * radius, -height * 0.5, -sin(i * step * M_PI / 180.0) * radius);
+		glVertex3f(cos((i + 1) * step * M_PI / 180.0) * radius, -height * 0.5, -sin((i + 1) * step * M_PI / 180.0) * radius);
+		glVertex3f(cos((i + 1) * step * M_PI / 180.0) * radius, height * 0.5, -sin((i + 1) * step * M_PI / 180.0) * radius);
 	}
+	glEnd();
 }
